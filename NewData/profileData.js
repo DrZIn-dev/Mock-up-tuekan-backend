17bc07bdaf5127eee5fa17f78da1b2d70ce00d0e
DrZin <pasawee.dev@gmail.com>
2020-03-24 14:16:37 +0700

    update to array

--- NewData/profileData.js
+++ NewData/profileData.js
@@ -1,43 +1,45 @@
 // add remaining coin.
-const profileData = {
-  firstName: "Werawit",
-  lastName: "Sirakunwat",
-  img: "",
-  coin: 150,
-  contact: [
-    {
-      id: 1,
-      name: "Facebook",
-      img: "",
-      link: "https://www.facebook.com/werawit.boat/"
-    },
-    {
-      id: 2,
-      name: "Instagram",
-      img: "",
-      link: "https://www.instagram.com/nawes_boat/"
-    },
-    {
-      id: 3,
-      name: "Youtube",
-      img: "",
-      link: "https://www.youtube.com/"
-    },
-    {
-      id: 4,
-      name: "Email",
-      img: "",
-      link: "#"
-    },
-    {
-      id: 5,
-      name: "Website",
-      img: "",
-      link: "#"
-    }
-  ],
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-};
+const profileData = [
+  {
+    firstName: "Werawit",
+    lastName: "Sirakunwat",
+    img: "",
+    coin: 150,
+    contact: [
+      {
+        id: 1,
+        name: "Facebook",
+        img: "",
+        link: "https://www.facebook.com/werawit.boat/"
+      },
+      {
+        id: 2,
+        name: "Instagram",
+        img: "",
+        link: "https://www.instagram.com/nawes_boat/"
+      },
+      {
+        id: 3,
+        name: "Youtube",
+        img: "",
+        link: "https://www.youtube.com/"
+      },
+      {
+        id: 4,
+        name: "Email",
+        img: "",
+        link: "#"
+      },
+      {
+        id: 5,
+        name: "Website",
+        img: "",
+        link: "#"
+      }
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+];
 
 module.exports = { profileData };
